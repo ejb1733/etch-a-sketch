2d5245127530b1f08f85b8b366839411f5f8faf9
ejb1733 <ethanjbillings@gmail.com>
2022-06-03 22:51:10 -0700

    Fix boxes not spreading equally
    - add nice background color

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-function genDivs(v){ 
-    var container = document.getElementById('container');
-    for(var i = 0; i < v; i++){ 
-      var row = document.createElement("div"); 
-      row.className = "row"; 
-      for(var x = 1; x <= v; x++){ 
-          var cell = document.createElement("div"); 
-          cell.className = "gridsquare"; 
-          row.appendChild(cell); 
-      } 
-      container.appendChild(row); 
-    } 
-    let nodelist = document.querySelectorAll('.gridsquare');
-    let gridArray = Array.from(nodelist);
-    console.log(gridArray.length);
-    
-    for (let n = 0; n < gridArray.length; n++) {
-        gridArray[n].addEventListener('mouseover', () => {
-            gridArray[n].style.backgroundColor = 'blue';
-        })
+const grid = document.getElementById('grid');
+
+const button = document.getElementById('prompt');
+
+let COLOR = 'beige';
+
+const GRID_SIZE_LIMIT = 128;
+
+button.addEventListener('click', () => {
+    let gridsize = prompt('Please provide a number n for an n x n grid');
+    while (gridsize > GRID_SIZE_LIMIT) {
+        alert('Given n is too large (> ' + GRID_SIZE_LIMIT + '). Please provide a smaller n.')
+        gridsize = prompt('Try again:');
     }
-  }
+    makeGrid(gridsize);
+})
 
+function makeGrid(size) {
+    while(grid.childNodes.length > 0) {
+        grid.removeChild(grid.lastChild);
+    }
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement('div');
+        row.className = 'row';
 
+        for (let j = 1; j <= size; j++) {
+            let square = document.createElement('div');
+            square.className = 'square';
+            square.style.width = `${500 / size}px`
+            square.style.height = square.style.width;
+            square.addEventListener('mouseover', () => {
+                square.style.backgroundColor = COLOR;
+            })
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
+    }
+}
+
+// function colorSelector() {
+
+// }
+
+// function shadr() {
+//     let gridArr = Array.from(grid.querySelectorAll('.square'));
+//     for (let i = 0; i < gridArr.length; i++) {
+//         gridArr[i].addEventListener('mouseover', () => {
+//             gridArr[i].style.backgroundColor +=
+//         })
+//     }
+// }
